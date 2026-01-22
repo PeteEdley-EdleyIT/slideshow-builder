e3cbeff3b64cf47a7f63f062c792b7736e520165
Peter Edley <peter@edleyit.com>
2026-01-22 16:50:40 +0000

    Implement initial minimum viable code for slideshow creation, ignoring output and image folders

---
+++ GEMINI.md
@@ -0,0 +1,17 @@
+# MCF Notices Video Automation
+
+This project is going to take a folder full of images in a jpeg format, and cobine them together into a video slide show.
+
+Here are a list of required features to work through one by one we will not be moving on to the next feature until the current on is complete and tested.
+
+# Development enviroment
+The development enviroment will be a nixos machine and we will developing in python, using gemini-cli.  The development enviroment will be set up using via a nix flake and all project dependancies will be managed by the flake instead of somthing like pip and a venv.
+
+# Deployment
+Final deployment will be via a docker image, the script will run via a cron drop within the docker image.
+
+# Features
+## Complete
+- [x] Initial minimun viable code - This needs to take images stored in a local folder and combine them into a video with each image being displayed for 10 seconds. The video needs to then be written out to another local folder
+
+## TODO
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          # Python interpreter
+          pkgs.python311
+
+          # venv and pip are included with python311, but we add the hook
+          # for automatic virtual environment management.
+          pkgs.python311Packages.venvShellHook
+
+          # System-level dependencies
+          pkgs.git
+          pkgs.ffmpeg
+          pkgs.gemini-cli
+
+          # Python packages managed by Nix
+          pkgs.python311Packages.numpy
+          pkgs.python311Packages.moviepy
+          pkgs.python311Packages.pillow
+        ];
+
+        # This hook will create and activate a virtual environment named '.venv'
+        # in your project directory when you enter the shell.
+        venvDir = ".venv";
+      };
+    };
+}
